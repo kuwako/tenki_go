ff4c2c6e0f1f73a6b5cc37575e5a6c6112bcd2e1
masaki_kuwako <m.kuwako0702@gmail.com>
2016-02-15 01:13:35 +0900

    天気をとってきて、テキスト生成まで完了したので、残りslackに投げるだけ

--- tenki.go
+++ tenki.go
@@ -8,6 +8,7 @@ import (
 
 // 東京の天気を取得するAPI
 var tenki_url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=130010"
+var tenki_text = ""
 
 func main() {
 	// urlにアクセス
@@ -27,7 +28,20 @@ func main() {
 		panic("json convert err")
 	}
 
-	fmt.Println(json.K("description").K("text"))
-	// 条件次第でpepper_botに投げる
+	fmt.Println(json.K("title"))
 
+	for _, content := range json.K("forecasts").RangeObjects() {
+		dateLabel, _ := content.K("dateLabel").AsString()
+		if dateLabel == "今日" {
+			today, _ := content.K("date").AsString()
+			weather, _ := content.K("telop").AsString()
+			description, _ := json.K("description").K("text").AsString()
+
+			tenki_text += today + "の東京の天気は " + weather + " です\n\n" + description
+		}
+	}
+
+	fmt.Println(tenki_text)
+
+	// 条件次第でpepper_botに投げる
 }
