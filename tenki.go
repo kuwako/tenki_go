af53930650556af5f2a25ed27af863db9ce86039
masaki_kuwako <m.kuwako0702@gmail.com>
2016-02-14 22:39:23 +0900

    天気の詳細を取れるようにした

--- tenki.go
+++ tenki.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/m0a/easyjson"
+	"net/http"
+)
+
+// 東京の天気を取得するAPI
+var tenki_url = "http://weather.livedoor.com/forecast/webservice/json/v1?city=130010"
 
 func main() {
-	fmt.Printf("Hello world\n")
+	// urlにアクセス
+	resp, err := http.Get(tenki_url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		panic(resp.StatusCode)
+	}
+
+	// jsonパース
+	json, err := easyjson.NewEasyJson(resp.Body)
+	if err != nil {
+		panic("json convert err")
+	}
+
+	fmt.Println(json.K("description").K("text"))
+	// 条件次第でpepper_botに投げる
+
 }
